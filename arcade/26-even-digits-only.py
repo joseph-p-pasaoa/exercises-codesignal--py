258dd0ed6fc52a015b0b6f20a3112512b5ca8281
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-11-30 09:17:11 -0500

    complete arcade/26-even-digits-only

---
+++ arcade/26-even-digits-only.py
@@ -0,0 +1,31 @@
+""" 26: EVEN DIGITS ONLY
+Check if all digits of the given integer are even.
+
+Example
+For n = 248622, the output should be
+evenDigitsOnly(n) = true;
+For n = 642386, the output should be
+evenDigitsOnly(n) = false.
+
+Input/Output
+[execution time limit] 4 seconds (py3)
+
+[input] integer n
+
+Guaranteed constraints:
+1 ≤ n ≤ 109.
+
+[output] boolean
+
+true if all digits of n are even, false otherwise.
+"""
+
+def evenDigitsOnly(n):
+    num = n
+    while num >= 1:
+        ones_digit = num % 10
+        if ones_digit % 2 != 0:
+            return False
+        num = math.floor(num / 10)
+    
+    return True
